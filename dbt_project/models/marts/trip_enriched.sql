ac12d080d52d4b158bf1499575f27085217190db
Marion <marion.lefebvre.thomas@gmail.com>
2026-05-06 15:01:44 +0200

    fix dbt et configuration postgres

--- dbt_project/models/marts/trip_enriched.sql
+++ dbt_project/models/marts/trip_enriched.sql
@@ -1,9 +1,7 @@
 select
     t.*,
     w.weather_category,
-    w.temperature,
-    w.humidity,
-    w.wind_speed
+    w.temperature
 from {{ ref('stg_fact_taxi_trips') }} t
 left join {{ ref('stg_dim_weather') }} w
-    on date_trunc('hour', t.pickup_datetime) = date_trunc('hour', w.weather_timestamp)
+    on t.pickup_hour = w.pickup_hour
